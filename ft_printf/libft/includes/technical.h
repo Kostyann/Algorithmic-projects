06699577595e74cfbe8dc85253978e203e36ddcb
Kostiantyn Merkulov <buycar713@gmail.com>
2018-12-08 20:05:34 +0200

    today's work upd2

---
+++ ft_printf/libft/includes/technical.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   technical.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmerkulo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/08 20:00:47 by kmerkulo          #+#    #+#             */
+/*   Updated: 2018/12/08 20:00:49 by kmerkulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TECHNICAL_H
+# define TECHNICAL_H
+
+#include "ft_printf.h"
+
+char		*ft_itoa_long(long long n);
+int			add_prefix(char **str, int c, size_t len);
+int			add_suffix(char **str, int c, size_t len);
+
+#endif
---
+++ ft_printf/technical.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   technical.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmerkulo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/08 19:58:18 by kmerkulo          #+#    #+#             */
+/*   Updated: 2018/12/08 19:58:20 by kmerkulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/includes/technical.h"
+
+static int	ft_len(long long nbr)
+{
+	int	len;
+
+	len = 0;
+	if (nbr < 0)
+		len++;
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char		*ft_itoa_long(long long n)
+{
+	int		neg;
+	int		len;
+	char	*str;
+
+	neg = 1;
+	len = ft_len(n);
+	if (n == 0)
+	{
+		if ((str = ft_strnew(1)))
+			str[0] = '0';
+		return (str);
+	}
+	if (n < 0)
+		neg = -1;
+	if ((str = ft_strnew(len)))
+	{
+		if (neg == -1)
+			str[0] = '-';
+		while (n != 0)
+		{
+			str[--len] = (n % 10) * neg + '0';
+			n /= 10;
+		}
+	}
+	return (str);
+}
+
+int			add_prefix(char **str, int c, size_t len)
+{
+	char *pref;
+	char *to_free;
+
+	pref = ft_strnew(len);
+	pref = ft_memset(pref, c, len);
+	to_free = *str;
+	*str = ft_strjoin(pref, *str);
+	free(to_free);
+	free(pref);
+	return (ft_strlen(*str));
+}
+
+int			add_suffix(char **str, int c, size_t len)
+{
+	char *suf;
+	char *to_free;
+
+	suf = ft_strnew(len);
+	suf = ft_memset(suf, c, len);
+	to_free = *str;
+	*str = ft_strjoin(*str, suf);
+	free(to_free);
+	free(suf);
+	return (ft_strlen(*str));
+}

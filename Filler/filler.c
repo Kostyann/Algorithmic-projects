14725f4fa23fd5cb687569b9c7dc3f63f7cc4eb6
Kostiantyn Merkulov <buycar713@gmail.com>
2018-12-27 16:04:44 +0200

    working on Filler day 4

--- Filler/filler.c
+++ Filler/filler.c
@@ -11,166 +11,18 @@
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
+#include "filler.h"
 
 #include <fcntl.h>
 
-char 	**make_field(int fd, int i, int j)
+int		try_place(int **piece, int **field, int x, int y)
 {
-	char	**res;
-	int		k;
-
-	k = -1;
-	res = (char**)malloc(sizeof(char*) * (i + 1));
-	while (++k < i && (get_next_line(fd, &res[k]) == 1))
-	{
-		if (res[k][0] >= '0' && res[k][0] <= '9')
-			ft_memmove(res[k], &res[k][4], j + 1);
-	}
-	res[k] = 0;
-	return (res);
-}
-
-void	fix_piece(char **piece, char num)
-{
-	int i = -1;
-	int j;
-
-	while (piece[++i])
-	{
-		j = -1;
-		while (piece[i][++j])
-			if (piece[i][j] == '*')
-				piece[i][j] = num;
-	}
-}
-
-void	map_field(char **field, char symb)
-{
+	int n;
 	int i;
 	int j;
-	int go = 1;
-	int num = -1;
-//	char me = (symb == 'O') ? 'X' : 'O';
 
-	while (go && num < 8)
-	{
-		go = 0;
-		i = -1;
-		num++;
-	/*	while (field[++i])
-			ft_printf("%s\n", field[i]);
-		ft_printf("go = %d\n", go); */
-		i = -1;
-		while (field[++i])
-		{
-			j = -1;
-			while (field[i][++j])
-			{
-				if (field[i][j] == '.')
-				{
-					/*if (i > 0)
-					{
-						if (j > 0 && field[i - 1][j - 1] != me)
-							go = 1;
-						else if (field[i - 1][j] != me)
-							go = 1;
-						else if (field[i - 1][j + 1] && field[i - 1][j + 1] != me)
-							go = 1;
-					}
-					else if (j > 0 && field[i][j - 1] != me)
-						go = 1;
-					else if (field[i][j + 1] && field[i][j + 1] != me)
-						go = 1;
-					else if (field[i + 1])
-					{
-						if (j > 0 && field[i + 1][j - 1] != me)
-							go = 1;
-						else if (field[i + 1][j] != me)
-							go = 1;
-						else if (field[i + 1][j + 1] && field[i + 1][j + 1] != me)
-							go = 1;
-					} */
-					go = 1;
-			//		ft_printf("go2 = %d\n", go);
-				}
-				else if (field[i][j] == symb || field[i][j] == symb +32 ||
-						field[i][j] == '0' + num)
-				{
-					if (i > 0)
-					{
-						if (j > 0 && field[i - 1][j - 1] == '.')
-							field[i - 1][j - 1] = '0' + num + 1;
-						if (field[i - 1][j] == '.')
-							field[i - 1][j] = '0' + num + 1;
-						if (field[i - 1][j + 1] && field[i - 1][j + 1] == '.')
-							field[i - 1][j + 1] = '0' + num + 1;
-					}
-					if (j > 0 && field[i][j - 1] == '.')
-						field[i][j - 1] = '0' + num + 1;
-					if (field[i][j + 1] && field[i][j + 1] == '.')
-						field[i][j + 1] = '0' + num + 1;
-					if (field[i + 1])
-					{
-						if (j > 0 && field[i + 1][j - 1] == '.')
-							field[i + 1][j - 1] = '0' + num + 1;
-						if (field[i + 1][j] == '.')
-							field[i + 1][j] = '0' + num + 1;
-						if (field[i + 1][j + 1] && field[i + 1][j + 1] == '.')
-							field[i + 1][j + 1] = '0' + num + 1;
-					}
-				}
-			}
-
-		}
-	}
-
-
-}
-
-char	find_me(char **piece)
-{
-	int i = -1;
-	int j;
-
-	while (piece[++i])
-	{
-		j = -1;
-		while (piece[i][++j])
-			if (!ft_isnumber(piece[i][j]))
-				return (piece[i][j]);
-	}
-	return (0);
-}
-
-#define ABS(x) ((x)<0 ? -(x) : (x))
-
-int		find_dist(char **piece, int coord[2], int x, int y)
-{
-	int i = -1;
-	int j;
-	int dist;
-
-	dist = 0;
-	while (piece[++i])
-	{
-		j = -1;
-		while (piece[i][++j])
-			if (!ft_isnumber(piece[i][j]))
-				dist += ABS(coord[0] + i - x + coord[1] + j - y);
-	}
-	return (dist);
-}
-
-int		try_place(char **piece, char **field, int x, int y)
-{
-	int n = 0;
-	int i = -1;
-	int j = -1;
-//	ft_printf("kaka\n");
-	/*	while (*field)
-				ft_printf("%s\n", *field++);
-			while (*piece)
-		ft_printf("%s\n", *piece++); */
+	i = -1;
+	n = 0;
 	while (piece[++i])
 	{
 		if (!(field[x + i]))
@@ -180,12 +32,11 @@ int		try_place(char **piece, char **field, int x, int y)
 		{
 			if (!(field[x + i][y + j]))
 				return (0);
-			if (piece[i][j] != '.')
+			if (piece[i][j] == -1)
 			{
-				if (field[x + i][y + j] == piece[i][j] ||
-					 field[x + i][y + j] == piece[i][j] + 32)
+				if (field[x + i][y + j] == -1)
 					n++;
-				else if (!(ft_isnumber(field[x + i][y + j])) && field[x + i][y + j] != '.')
+				else if (field[x + i][y + j] == -2)
 					return (0);
 			}
 		}
@@ -195,27 +46,25 @@ int		try_place(char **piece, char **field, int x, int y)
 	return (1);
 }
 
-int		ft_closer(char **piece, char **field, int prime[2], int temp[2])
+int		ft_closer(int **piece, int **field, int prime[2], int temp[2])
 {
-	char me = find_me(piece);
-	char he = (me == 'O') ? 'X' : 'O';
-	int i = -1;
+	int i;
 	int j;
 	int p_dist;
 	int t_dist;
 
+	i = -1;
 	p_dist = 0;
 	t_dist = 0;
-	while (field[++i])
+	while (piece[++i])
 	{
 		j = -1;
-		while (field[i][++j]) {
-			if ((field[i][j] == he || field[i][j] == he + 32) &&
-				((j > 0 && ft_isnumber(field[i][j - 1])) ||
-				(field[i][j + 1] && ft_isnumber(field[i][j + 1]))))
+		while (piece[i][++j])
+		{
+			if (piece[i][j] == -1)
 			{
-				p_dist += find_dist(piece, prime, i, j);
-				t_dist += find_dist(piece, temp, i, j);
+				p_dist += field[prime[0] + i][prime[1] + j];
+				t_dist += field[temp[0] + i][temp[1] + j];
 			}
 		}
 	}
@@ -224,14 +73,14 @@ int		ft_closer(char **piece, char **field, int prime[2], int temp[2])
 	return (0);
 }
 
-void	place_piece(char **field, char **piece)
+void	place_piece(int **field, int **piece)
 {
 	int	prime[2] = {-1, -1};
 	int temp[2] = {-1, -1};
-	int i = -1;
-	int j = -1;
+	int i;
+	int j;
 
-
+	i = -1;
 	while (field[++i])
 	{
 		j = -1;
@@ -261,38 +110,34 @@ int		main(void)
 {
 	char	*line;
 	int		fd;
-	int 	xyf[2] = {0, 0};
-	int 	xyp[2] = {0, 0};
-	char 	pl_num;
+	int 	xy[2] = {0, 0};
 	char	en_num;
-	char 	**field;
-	char 	**piece;
+	int 	**field;
+	int 	**piece;
 
 //	fd = open("test.txt", O_RDONLY);
 	fd = 0;
 
 	if ((get_next_line(fd, &line) == 1))
 	{
-		pl_num = line[10];
+		en_num = line[10];
 		free(line);
-		pl_num = (pl_num == '1') ? 'O' : 'X';
-		en_num = (pl_num == 'O') ? 'X' : 'O';
+		en_num = (en_num == '1') ? 'X' : 'O';
 	}
 	while ((get_next_line(fd, &line) == 1))
 		{
-			xyf[0] = ft_atoi(&line[8]);
-			xyf[1] = ft_atoi(&line[8 + ft_digits(xyf[0]) + 1]);
+			xy[0] = ft_atoi(&line[8]);
+			xy[1] = ft_atoi(&line[8 + ft_digits(xy[0]) + 1]);
 			free(line);
 			get_next_line(fd, &line);
-			field = make_field(fd, xyf[0], xyf[1]);
+			field = make_field(en_num, fd, xy[0], xy[1]);
 			free(line);
 			get_next_line(fd, &line);
-			xyp[0] = ft_atoi(&line[6]);
-			xyp[1] = ft_atoi(&line[6 + ft_digits(xyp[0]) + 1]);
-			piece = make_field(fd, xyp[0], xyp[1]);
+			xy[0] = ft_atoi(&line[6]);
+			xy[1] = ft_atoi(&line[6 + ft_digits(xy[0]) + 1]);
+			piece = make_field(en_num, fd, xy[0], xy[1]);
 			free(line);
-			fix_piece(piece, pl_num);
-			map_field(field, en_num);
+			map_field(field);
 			place_piece(field, piece);
 		}
 
---
+++ Filler/tricks.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tricks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmerkulo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/27 12:12:53 by kmerkulo          #+#    #+#             */
+/*   Updated: 2018/12/27 12:12:55 by kmerkulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/includes/libft.h"
+#include "filler.h"
+
+int 	**make_field(char en_num, int fd, int i, int j)
+{
+	char	*line;
+	int		**res;
+	int		k;
+	int 	n;
+
+	k = -1;
+	res = (int**)malloc(sizeof(int*) * (i + 1));
+	while (++k < i && (get_next_line(fd, &line) == 1))
+	{
+		if (line[0] >= '0' && line[0] <= '9')
+			ft_memmove(line, &line[4], j + 1);
+		res[k] = (int*)malloc(sizeof(int) * (j + 1));
+		n = -1;
+		while (line[++n])
+		{
+			if (line[n] == '.')
+				res[k][n] = 42;
+			else if (line[n] == en_num || line[n] == en_num + 32)
+				res[k][n] = -2;
+			else
+				res[k][n] = -1;
+		}
+		res[k][n] = 0;
+	}
+	res[k] = 0;
+	return (res);
+}
+
+void	map_field(int **field)
+{
+	int i;
+	int j;
+	int go = 1;
+	int num = -1;
+
+	while (num < 70)
+	{
+		go = 0;
+		i = -1;
+		num++;
+		while (field[++i])
+		{
+			j = -1;
+			while (field[i][++j])
+			{
+				if (field[i][j] == -2 || field[i][j] == num)
+				{
+					if (i > 0)
+					{
+						if (j > 0 && field[i - 1][j - 1] == 42)
+							field[i - 1][j - 1] = num + 1;
+						if (field[i - 1][j] == 42)
+							field[i - 1][j] = num + 1;
+						if (field[i - 1][j + 1] && field[i - 1][j + 1] == 42)
+							field[i - 1][j + 1] = num + 1;
+					}
+					if (j > 0 && field[i][j - 1] == 42)
+						field[i][j - 1] = num + 1;
+					if (field[i][j + 1] && field[i][j + 1] == 42)
+						field[i][j + 1] = num + 1;
+					if (field[i + 1])
+					{
+						if (j > 0 && field[i + 1][j - 1] == 42)
+							field[i + 1][j - 1] = num + 1;
+						if (field[i + 1][j] == 42)
+							field[i + 1][j] = num + 1;
+						if (field[i + 1][j + 1] && field[i + 1][j + 1] == 42)
+							field[i + 1][j + 1] = num + 1;
+					}
+				}
+			}
+
+		}
+	}
+}

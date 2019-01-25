5ae2576ad8af65fbf90c1cfee841d22a00234f6d
Kostiantyn Merkulov <buycar713@gmail.com>
2019-01-25 17:05:44 +0200

    Lem-in first commit. Working data structure and pretty much working path-finder

--- libft/libft_test_jtoty/my_config.sh
+++ libft/libft_test_jtoty/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=~/ls
+PATH_LIBFT=../libft/
 
 #If you use a relative path, don't put double quotes.
 #For example :
